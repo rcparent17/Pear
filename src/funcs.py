6095aa16d1ea1b20d5b9a8359f36e27e4dadec72
Reilly Parent <reilly.c.parent@gmail.com>
2020-01-13 09:38:56 -0500

    began basic functions and modularization

--- src/funcs.py
+++ src/funcs.py
@@ -1,2 +1,54 @@
+import sys, os
+
+global pFile
+
+def setPear(pear):
+	global pFile
+	pFile = pear
+	
+
 def execute(cmd):
-    args = cmd.split()
+	args = cmd.split()
+	a = args[0]
+	if a=="exit":
+		sys.exit(0)
+	elif a=="peek":
+		peek()
+	elif a=="mem":
+		if not len(args)==2:
+			sys.stdout.write("Invalid syntax. Proper usage: mem [value]\n")
+			sys.stdout.flush()
+		else:
+			mem(args)
+	elif a=="memclr":
+		memclr()
+	else:
+		sys.stdout.write("Command not found: " + args[0] + "\n")
+		sys.stdout.flush()
+
+def peek():
+	tmp = open(pFile, "r")
+	for line in tmp.readlines():
+		sys.stdout.write(line)
+		sys.stdout.flush()
+	tmp.close()
+
+def mem(args):
+	tmp = open(pFile, "a+")
+	os.system("cat " + pFile + " | grep -i \'mem\' > tmp.p")
+	tmp2 = open("tmp.p", "r+")
+	length = len(tmp2.readlines())
+	tmp2.close()
+	os.system("rm -f tmp.p")
+	try:
+		test = int(args[1])
+		tmp.write("mem|0x" + str(length) + "->" + args[1] + "\n")
+		tmp.close()
+	except Exception as e:
+		sys.stdout.write(str(e))
+		sys.stdout.write("Invalid syntax. Value needs to be numeric.\n")
+		sys.stdout.flush()
+
+def memclr():
+	tmp = open(pFile, "w")
+	
--- src/pear.py
+++ src/pear.py
@@ -1,144 +1,145 @@
+import funcs
+
 import os
 import sys
 import os.path
 from os import path
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
 
 class pear:
-    osVer = "0.0.1a"
-    memory = {}
-    users = {}
-    currUser = None
-    files = {}
-    image = None
-    name = ""
+	osVer = "0.0.1a"
+	memory = {}
+	users = {}
+	currUser = None
+	files = {}
+	image = None
+	name = ""
 
-    def __init__(self):
-        os.system("clear")
-        self.start()
-        for line in self.image.readlines():
-            parts = line.split('|')
-            if parts[0] == "mmry":
-                ptr = parts[1].split("->")
-                self.memory[ptr[0]] = ptr[1]
-            elif parts[0] == "user":
-                unpw = parts[1].split("::")
-                self.users[unpw[0].strip()] = unpw[1].strip()
-            elif parts[0] == "file":
-                info = parts[1].split(">>")
-                files[info[0]] = info[1]
-        self.signin()
-        os.system("clear")
-        self.awaitCommand()
+	def __init__(self):
+		os.system("clear")
+		self.start()
+		for line in self.image.readlines():
+			parts = line.split('|')
+			if parts[0] == "mem":
+				ptr = parts[1].split("->")
+				self.memory[eval(ptr[0])] = int(ptr[1])
+			elif parts[0] == "usr":
+				unpw = parts[1].split("::")
+				self.users[unpw[0].strip()] = unpw[1].strip()
+			elif parts[0] == "fil":
+				info = parts[1].split(">>")
+				files[info[0]] = info[1]
+		self.signin()
+		os.system("clear")
+		self.awaitCommand()
 
-    def signin(self):
-        signedIn = False
-        sys.stdout.write("Username: ")
-        sys.stdout.flush()
-        un = input()
-        if un in self.users.keys():
-        	sys.stdout.write("Password: ")
-        	sys.stdout.flush()
-        	pw = input()
-        	for u, p in self.users.items():
-            		sys.stdout.write(un + " " + u + " " + pw + " " + p)
-            		sys.stdout.flush()
-            		if un.strip() == u and pw.strip() == p:
-                		self.currUser = un
-                		signedIn = True
-                		print("omg")
-                		break
-        if not signedIn:
-            sys.stdout.write("Sign-in failed. Please try again.\n\n")
-            sys.stdout.flush()
-            self.signin()
+	def signin(self):
+		signedIn = False
+		sys.stdout.write(str(self.users) + "\n")
+		sys.stdout.flush()
+		sys.stdout.write("Username: ")
+		sys.stdout.flush()
+		un = input()
+		if un in self.users.keys():
+			sys.stdout.write("Password: ")
+			sys.stdout.flush()
+			pw = input()
+			for u, p in self.users.items():
+				sys.stdout.write(un + " " + u + " " + pw + " " + p)
+				sys.stdout.flush()
+				if un == u and pw == p:
+					self.currUser = un
+					signedIn = True
+					break
+		if not signedIn:
+			sys.stdout.write("Sign-in failed. Please try again.\n\n")
+			sys.stdout.flush()
+			self.signin()
 
-    def loadImage(self):
-        sys.stdout.write("Image file: ")
-        sys.stdout.flush()
-        newImg = input()
-        try:
-            self.image = open(newImg, "r+")
-            self.name = newImg
-        except Exception as e:
-            self.imgNotFound(newImg)
+	def loadImage(self):
+		sys.stdout.write("List of current directory: ")
+		sys.stdout.flush()
+		os.system("ls")
+		sys.stdout.write("Choose a Pear file: ")
+		sys.stdout.flush()
+		newImg = input()
+		try:
+			self.image = open(newImg, "r+")
+			self.name = newImg
+			funcs.setPear(newImg)
+		except Exception as e:
+			self.imgNotFound(newImg)
 
-    def imgNotFound(self, imgFile):
-        sys.stdout.write("Image not found: " + imgFile + ". Would you like to (1) perform first time setup or (2) select a different image?")
-        sys.stdout.flush()
-        choice = input()
-        if choice=="1":
-            self.initSetup()
-            self.loadImage()
-        elif choice=="2":
-            self.loadImage()
-        else:
-            while not choice=="1" and not choice=="2":
-                sys.stdout.write("Please select 1 or 2. ")
-                sys.stdout.flush()
-                choice = input()
-                if choice=="1":
-                    self.initSetup()
-                elif choice=="2":
-                    self.loadImage()
-
-
-    def initSetup(self):
-        sys.stdout.write("Welcome to PearOS v" + self.osVer + "! Please enter the name of the file you would like to store the image in (excluding .pear): ")
-        sys.stdout.flush()
-        img = input()
-        tempImg = open(img + ".pear", "w")
-        sys.stdout.write("Enter your preferred username: ")
-        sys.stdout.flush()
-        un = input()
-        sys.stdout.write("Enter your preferred password: ")
-        sys.stdout.flush()
-        pw = input()
-        tempImg.write("user|" + un + "::" + pw + "\n")
-        sys.stdout.write("User account created and stored.\n")
-        sys.stdout.flush()
-
-    def start(self):
-        sys.stdout.write("Would you like to (1) create or (2) load an image?")
-        sys.stdout.flush()
-        choice = input()
-        if choice=="1":
-            self.initSetup()
-            self.loadImage()
-        elif choice=="2":
-            self.loadImage()
-        else:
-            while not choice=="1" and not choice=="2":
-                sys.stdout.write("Please select 1 or 2.")
-                sys.stdout.flush()
-                choice = input()
-                if choice=="1":
-                    self.initSetup()
-                    self.loadImage()
-                elif choice=="2":
-                    self.loadImage()
+	def imgNotFound(self, imgFile):
+		sys.stdout.write("Image not found: " + imgFile + ". Would you like to (1) perform first time setup or (2) select a different image?")
+		sys.stdout.flush()
+		choice = input()
+		if choice=="1":
+			self.initSetup()
+			self.loadImage()
+		elif choice=="2":
+			self.loadImage()
+		else:
+			while not choice=="1" and not choice=="2":
+				sys.stdout.write("Please select 1 or 2. ")
+				sys.stdout.flush()
+				choice = input()
+				if choice=="1":
+					self.initSetup()
+				elif choice=="2":
+					self.loadImage()
 
 
-    def awaitCommand(self):
-        sys.stdout.write(bcolors.OKBLUE + self.currUser + bcolors.ENDC + "@" + bcolors.BOLD + self.name + bcolors.ENDC + bcolors.OKGREEN+ " (PearOS_v" + self.osVer + ") " + bcolors.ENDC + "> ")
-        sys.stdout.flush()
-        cmd = input()
-        self.execute(cmd)
-        self.awaitCommand()
+	def initSetup(self):
+		sys.stdout.write("Welcome to Pear v" + self.osVer + "! Please enter the name of the file you would like to store the image in (excluding .pear): ")
+		sys.stdout.flush()
+		img = input()
+		tempImg = open(img + ".pear", "w")
+		sys.stdout.write("Enter your preferred username: ")
+		sys.stdout.flush()
+		un = input()
+		sys.stdout.write("Enter your preferred password: ")
+		sys.stdout.flush()
+		pw = input()
+		tempImg.write("usr|" + un + "::" + pw + "\n")
+		sys.stdout.write("User account created and stored.\n")
+		sys.stdout.flush()
 
-    def execute(self, cmd):
-        sys.stdout.write("Executing command: " + cmd + "\n")
-        if cmd=="exit":
-            sys.exit(0)
-        sys.stdout.flush()
+	def start(self):
+		sys.stdout.write("Would you like to (1) create or (2) load an image? ")
+		sys.stdout.flush()
+		choice = input()
+		if choice=="1":
+			self.initSetup()
+			self.loadImage()
+		elif choice=="2":
+			self.loadImage()
+		else:
+			while not choice=="1" and not choice=="2":
+				sys.stdout.write("Please select 1 or 2. ")
+				sys.stdout.flush()
+				choice = input()
+				if choice=="1":
+					self.initSetup()
+					self.loadImage()
+				elif choice=="2":
+					self.loadImage()
+
+
+	def awaitCommand(self):
+		sys.stdout.write(bcolors.OKBLUE + self.currUser + bcolors.ENDC + "@" + bcolors.BOLD + self.name + bcolors.ENDC + bcolors.OKGREEN+ "(PearOS_v" + self.osVer + ")" + bcolors.ENDC + "$ ")
+		sys.stdout.flush()
+		cmd = input()
+		funcs.execute(cmd)
+		self.awaitCommand()
 
 pear = pear()
